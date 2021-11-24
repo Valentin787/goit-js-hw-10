66813bbe6a89501aca0ae81469c23a592c107b5d
Valentin787 <89661234+Valentin787@users.noreply.github.com>
2021-11-24 23:01:00 +0200

    hm-10

---
+++ src/js/index.js
@@ -0,0 +1,79 @@
+import '../css/styles.css';
+import debounce from 'lodash.debounce';
+
+import notiflix from 'notiflix';
+
+import fetchCountries from './service/api-services';
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
+
+const createUsersMarkupLi = obj => {
+  const markup = obj
+    .map(
+      ({ name: { official }, lags: { svg } }) => `<div class ="js-list">
+        <div>
+        <img src="${svg}" alt="flag" width="40" class ="js-img">
+        </div>
+        <h1>${official}</h1>
+        </div>`,
+    )
+    .join('');
+  refs.info.innerHTML = '';
+  refs.list.innerHTML = markup;
+};
+
+const createMarkupList = dataList => {
+  console.log(dataList);
+  const markupList = dataList
+    .map(({ name: { official }, capital, population, flags: { svg }, languages }) => {
+      const languagesKey = Object.values(languages).join(',');
+      return `<div class ="js-list">
+        <div>
+        <img src="${svg}" alt="flag" width="40" class ="js-img">
+        </div>
+        <h1>${official}</h1>
+        </div>
+<ul>
+  <li class="js-item">Capital:<span class ="js-icon">${capital}</span></li>
+  <li class="js-item">Population:<span class ="js-icon">${population}</span></li>
+  <li class="js-item">Languages:<span class ="js-icon">${languagesKey}</span></li>
+</ul>
+`;
+    })
+    .join('');
+  refs.list.innerHTML = '';
+  refs.info.innerHTML = markupList;
+};
+
+const handleCountryInput = e => {
+  const inputName = e.target.value.trim();
+
+  fetchCountries(inputName)
+    .then(data => {
+      if (data.length > 10) {
+        notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`);
+        return;
+      }
+      if (data.length < 10 && data.length > 1) {
+        createUsersMarkupLi(data);
+      }
+      if (data.length === 1) {
+        createMarkupList(data);
+      } else {
+        return false;
+      }
+    })
+    .catch(err => {
+      refs.info.innerHTML = '';
+      refs.list.innerHTML = '';
+      notiflix.Notify.failure(`Oops, there is no country with that name`);
+    });
+};
+
+refs.input.addEventListener('input', debounce(handleCountryInput, DEBOUNCE_DELAY));
---
+++ src/js/service/api-services.js
@@ -0,0 +1,15 @@
+const BASE_URL = 'https://restcountries.com';
+
+const fetchCountries = name => {
+  return fetch(`${BASE_URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(
+    response => {
+      if (!response.ok) {
+        return Promise.reject(new Error(error));
+
+      }
+      return response.json();
+    },
+  );
+};
+
+export default fetchCountries;
